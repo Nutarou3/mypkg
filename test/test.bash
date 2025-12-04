cf8ec26519ba5af580b150c46a59c640912719fd
Nutarou3 <nutaryau3@gmail.com>
2025-12-04 13:32:47 +0900

    add test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# ベースディレクトリを設定
+# GitHub Actionsでは通常 /root になる
 dir=~
 [ "$1" != "" ] && dir="$1"
-
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_liten.launch.py > /tmp/mypkg.log
+source install/setup.bash
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Liten: 10'
-
